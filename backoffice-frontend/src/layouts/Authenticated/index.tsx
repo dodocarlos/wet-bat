a97b32c4e8a923392100c5560125b68cc072c1a4
Carlos E. Pegoretti <carlosdodo.pegoretti@gmail.com>
2022-10-23 23:06:13 -0300

    adjust home page minHeight

--- backoffice-frontend/src/layouts/Authenticated/index.tsx
+++ backoffice-frontend/src/layouts/Authenticated/index.tsx
@@ -7,7 +7,7 @@ import MyAppBar from '../../components/MyAppBar'
 export default function AuthenticatedLayout({ children }: React.PropsWithChildren) {
   return (
     <Paper>
-      <Box sx={{ display: 'flex', width: '100%', height: '100%' }}>
+      <Box sx={{ display: 'flex', width: '100%', height: '100%', minHeight: '100vh' }}>
         <MyAppBar />
         <MyDrawer />
         <Box
